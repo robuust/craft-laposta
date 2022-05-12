5fc07dd0ca58273e5aa3d701488a143df161b3ce
Bob Olde Hampsink <bob@robuust.digital>
2022-05-12 08:58:18 +0200

    Add field

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,9 +2,25 @@
 
 namespace robuust\laposta;
 
+use craft\events\RegisterComponentTypesEvent;
+use craft\services\Fields;
+use robuust\laposta\fields\LaPosta;
+use yii\base\Event;
+
 /**
  * LaPosta plugin.
  */
 class Plugin extends \craft\base\Plugin
 {
+    /**
+     * Initializes the plugin.
+     */
+    public function init()
+    {
+        parent::init();
+
+        Event::on(Fields::class, Fields::EVENT_REGISTER_FIELD_TYPES, function (RegisterComponentTypesEvent $event) {
+            $event->types[] = LaPosta::class;
+        });
+    }
 }
---
+++ src/fields/LaPosta.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace robuust\laposta\fields;
+
+use craft\fields\Dropdown;
+
+/**
+ * LaPosta Field.
+ *
+ * @author    Bob Olde Hampsink <bob@robuust.digital>
+ * @copyright Copyright (c) 2022, Robuust
+ * @license   MIT
+ *
+ * @see       https://robuust.digital
+ */
+class LaPosta extends Dropdown
+{
+}
